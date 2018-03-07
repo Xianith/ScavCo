fa45b146c7aacff785d3db0f6996622676138632
root <root@ip-172-30-0-196.us-west-2.compute.internal>
2018-03-07 19:46:02 +0000

    Added Gapi data

---
+++ js/util/gapiData.js
@@ -0,0 +1,3 @@
+export var API_KEY = 'AIzaSyBuiD7FAD9c7PAj0Np_ZwVsiHLbyTLKoBk';
+export var CLIENT_ID = '268531681980-bqf0gvhlgt0op2u526ts5ppvoov3hfk3.apps.googleusercontent.com';
+export var SCOPE = 'https://www.googleapis.com/auth/spreadsheets';
