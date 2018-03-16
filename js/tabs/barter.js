0ab3cec1c741a5d19f5e1597b710c800a30dc08c
root <root@ip-172-30-0-196.us-west-2.compute.internal>
2018-03-16 21:45:51 +0000

    Initial sheet change

--- js/tabs/barter.js
+++ js/tabs/barter.js
@@ -1,38 +1,11 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 
-import '../../css/tabs.css';
-import mods from '../../assets/categories/mods.png'
-import { navMenu } from '../menu'
-import { initGapi, loadGapi  } from '../util/gapiData.js'
-import { jsUcfirst } from '../app'
-
-var SHEET_ID = '1Yk-VriCy_8vDH4V9SsLwRYxem2mkzoDrULiaHZY5UGQ'
-var RANGE = 'A3:L189'
-
-var dnr = [3,4,5,7,8] //Rows that should not be rendered
-
-var barterArray = [{"name":"prapor","status":"barter-btn-active"},
-{"name":"therapist","status":""},
-{"name":"fence","status":""},
-{"name":"skier","status":""},
-{"name":"peacekeeper","status":""},
-{"name":"tradeable","status":""},
-{"name":"buyable","status":""}];
-
-var catArray = ['tradeable','buyable'];
+import CustomSortTable from '../util/sheetDisplay'
 
 export default class Barter extends Component {
   constructor(props) {
     super();
-    this.onClick = this.handleClick.bind(this);
-  }
-
-  componentWillMount() {
-    loadGapi()
-    var goog = initGapi(SHEET_ID, RANGE, (resp) => {
-      tableStylize(resp, 'barter-table');
-    }); 
   }
 
   componentDidMount() {
@@ -42,166 +15,10 @@ export default class Barter extends Component {
     document.getElementById('MainMenu').style.display = 'block';
   }
 
-  handleClick(event) {
-    const {id} = event.target;
-    navMenu(id,'barter');
+  render(){
+    return(
+      <CustomSortTable pathname='bartering' />
+    )
   }
 
-  render() {
-    return (<div>
-      <div className="loading-div">Loading...</div>  
-      <div className="jumbotron contentcontainer tab-container" id="Bartering" style={{display:"none"}}>
-        <div>
-          <div className="sub-nav-menu"><center>
-          {barterArray.map((btn) =>
-                 <button id={'sort-'+btn.name} className={btn.status} onClick={this.onClick} key={'barter-btn-'+btn.name}>{jsUcfirst(btn.name)}</button>
-              )}
-          </center></div>
-
-        <table data-toggle="table" id="barter-table" className="sheets-table table table-fixed table-hover table-responsive table-sm" cellSpacing="0" width="100%">
-          <thead></thead>
-          <tbody></tbody>
-        </table>
-
-        <span>Data is pulled from the following <a href={'https://docs.google.com/spreadsheets/d/'+SHEET_ID}>spreadsheet</a>. <i>Which is maintained by <a href="https://www.reddit.com/user/Gieke85">/u/Gieke85</a></i></span>
-      </div>
-    </div>
-  </div>);
-  }
-}
-
-function updateSignInStatus(isSignedIn) {
-  if (isSignedIn) {
-    makeApiCall();
-  } else { makeApiCall(); }
-}
-
-function tableStylize(response, table) {
-  document.getElementById(table).innerHTML = '<thead></thead><tbody></tbody>';
-   var range = response.result;
-    if (range.values.length > 0) {
-      for (var i = 0; i < range.values.length; i++) {
-        var row = range.values[i];
-        if (i == 0) { styleHeader(row,); } else { styleRow(row); }
-      }
-      document.getElementsByClassName('loading-div')[0].remove();
-      document.getElementById('Bartering').style.display = 'block';
-    } else {
-      tableFill('No data found.');
-    }
-}
-
-function styleHeader(row) {
-  var table = document.getElementById('barter-table');
-  var tr = document.createElement('tr')
-
-  for (var y = 0; y < 12; y++) {
-    var thead = table.getElementsByTagName('thead')[0];
-    var th = document.createElement('th');
-
-    if (y == 0) {
-      var text = document.createTextNode(row[1]);
-    } else if (y == 1) { 
-      var text = document.createTextNode(row[0]);
-      th.colSpan = 2;
-    } else if (y == 2) {
-        var text = document.createTextNode('Sell Price');
-        th.colSpan = 2;
-    } else if (y == 6) {
-        var text = document.createTextNode('Value (WxH:Slot Total) ');
-        th.colSpan = 2;
-    } else {  
-      var text = document.createTextNode(row[y]);
-    }
-
-    if (dnr.indexOf(y) > -1) { } 
-    else {
-      th.appendChild(text);
-      tr.appendChild(th);
-      thead.appendChild(tr);
-    }
-  }
-}
-
-function styleRow(row) {
-  var table = document.getElementById('barter-table');
-  var tr = document.createElement('tr'); 
-
-  for (var y = 0; y < 12; y++) {
-    var tbody = table.getElementsByTagName('tbody')[0];
-    var td = document.createElement('td');
-    var text = document.createTextNode(row[y]);
-
-    for (var a = 0; a < barterArray.length; a++) {
-      if (row.indexOf(jsUcfirst(barterArray[a].name)) > -1) {
-        tr.className = barterArray[a].name + '-row barter-row';
-        if (barterArray[a].name == 'prapor') { tr.style.display = 'table' } else {
-          tr.style.display = 'none'; }
-      } else if (tr.className.length == 0) {
-        tr.className = 'other-barter-row ammo-row';
-        tr.style.display = 'none';
-      }
-    }
-
-    if (y == 6) {td.className = 'success';}
-
-    if (typeof row[10] != 'undefined' && row[10].length > 0) {tr.className = tr.className + ' tradeable-row'; }
-    if (typeof row[9] != 'undefined' && row[9].length > 0) {tr.className = tr.className + ' quest-row'; }
-    if (typeof row[11] != 'undefined' && row[11].length > 0) {tr.className = tr.className + ' buyable-row'; }
-    if (typeof row[y] == 'undefined') { 
-      var td = document.createElement('td');
-      var text = document.createTextNode('');
-    }
-
-    var value = document.createElement('span');
-
-    switch (y) {
-      case 0:
-        td.appendChild(styleCat(row[1])); //Swap Category to row 0
-        break;
-      case 1:
-        td.colSpan = 2;
-        let cleanTitle = row[0].replace(/\([^)]*\)/gi,'');
-        td.innerHTML = '<a target="_blank" href="https://escapefromtarkov.gamepedia.com/'+cleanTitle+'">'+row[0]+'</a>'; //Swap Item to row 1
-        break;
-      case 2:
-        if (row[3] == row[4]) { 
-          value.innerHTML = row[2] +'₽';
-          } else {
-          value.innerHTML = '<span title="Maximum Price" class="alert-success"><span class="glyphicon glyphicon-chevron-up" aria-hidden="true"></span><span class="sell-min">' + 
-            row[3] + '₽</span> <br /> <span title="Average Price" class="alert-info"><span class="glyphicon glyphicon-minus" aria-hidden="true"></span><span class="sell-max">' + 
-            row[4] + '₽</span> <br /> <span title="Minimum Price" class="alert-danger"><span class="glyphicon glyphicon-chevron-down" aria-hidden="true"></span><span class="sell-avg">' + 
-            row[2] + '₽</span></span>';
-        }
-        td.appendChild(value);
-        td.colSpan = 2;
-        tr.appendChild(td);
-        tbody.appendChild(tr);
-        break;
-      case 6:
-        value.innerHTML = row[6] +  
-          '₽ (' + row[7] + ':' + row[5] + ')';
-        td.colSpan = 2;
-        td.appendChild(value);
-        break;
-      default:
-        td.appendChild(text);
-    }
-
-    if (dnr.indexOf(y) > -1) { 
-    } else {
-      tr.appendChild(td);
-      tbody.appendChild(tr);
-    }
-  }
-}
-
-function styleCat(row) {
-  if (row == 'Mod') {
-    var img = document.createElement('img');
-    img.src = mods;
-    img.alt = row;
-    img.title = row;
-    return img;
-  } else { return document.createTextNode(row); }
 }
---
+++ js/util/sheetDisplay.js
@@ -0,0 +1,443 @@
+/* eslint max-len: 0 */
+import React from 'react';
+import ReactTable from 'react-table'
+import { initGapi, loadGapi } from '../util/gapiData'
+import { nameClean } from '../data/sheetStyles'
+// import {sArray} from '../data/headers'
+
+import '../../css/sheets.css'
+import '../../../node_modules/react-bootstrap-table/dist/react-bootstrap-table-all.min.css';
+
+function priceExpand(t) {
+  console.log(t)
+}
+
+var sArray = [{
+  "name":"Official",
+  "color":"#d7b100",
+  "id":"1l_8zSZg-viVTZ2bavMEIIKhix6mFTXuVHWcNKZgBrjQ",
+  "range":"A1:J67",
+  "credit":"",
+  "header": [{
+      Header: 'Name',
+      accessor: 'name', // String-based value accessors!
+      width: 250, 
+      Cell: props => {let name = nameClean(props.value);
+        let url = "https://escapefromtarkov.gamepedia.com/" + name;
+
+        return <a target="_blank" href={url}>{name}</a>}
+    }, {
+      Header: 'Price',
+      accessor: 'price',
+      filterable: false,
+      filterall: false,
+      minWidth: 100,
+      sortFunc:  (a, b) => {
+                           return a > b;
+                       }, 
+      Cell: props => <span className='number'>{props.value} ₽</span> // Custom cell components!
+    }, {
+      accessor: 'damage', // Required because our accessor is not a string
+      Header: 'Damage',
+      filterable: false,
+      filterall: false,
+      minWidth: 100
+    }, {
+      accessor: 'armorpen', // Required because our accessor is not a string
+      Header: 'Armor Pen',
+      filterable: false,
+      filterall: false,
+      minWidth: 100,
+      sortFunc:  (a, b) => {
+                         if (a.length === b.length) {
+                           return a > b ? 1 : -1;
+                         }
+                         return a.length > b.length ? 1 : -1;
+                       }, 
+    }, {
+      accessor: 'speed', // Required because our accessor is not a string
+      Header: 'Speed',
+      filterable: false,
+      filterall: false,
+      minWidth: 100,
+      sortFunc:  (a, b) => {
+                         if (a.length === b.length) {
+                           return a > b ? 1 : -1;
+                         }
+                         return a.length > b.length ? 1 : -1;
+                       }, 
+    }, {
+      accessor: 'richo', // Required because our accessor is not a string
+      Header: 'Richochet %',
+      filterable: false,
+      filterall: false,
+      minWidth: 100,
+      sortFunc:  (a, b) => {
+                         if (a.length === b.length) {
+                           return a > b ? 1 : -1;
+                         }
+                         return a.length > b.length ? 1 : -1;
+                       }, 
+    }, {
+      accessor: 'frag', // Required because our accessor is not a string
+      Header: 'Frag %',
+      filterable: false,
+      filterall: false,
+      minWidth: 100,
+      sortFunc:  (a, b) => {
+                         if (a.length === b.length) {
+                           return a > b ? 1 : -1;
+                         }
+                         return a.length > b.length ? 1 : -1;
+                       }, 
+    }, {
+      accessor: 'meta', // Required because our accessor is not a string
+      Header: 'Usefullness',
+      filterable: false,
+      filterall: false,
+      minWidth: 100,
+      sortFunc:  (a, b) => {
+                         if (a.length === b.length) {
+                           return a > b ? 1 : -1;
+                         }
+                         return a.length > b.length ? 1 : -1;
+                       }, 
+    }],
+  "dnr": [7]
+},{
+  "name":"Unofficial",
+  "color":"gray",
+  "credit":"",
+  "id":"1t4lA1NCQmM0NpTprGJT7rDVKZXHmQuPzQK5Ul23UoVo",
+  "range":"A1:P72",
+  "header": [{
+      Header: 'Name',
+      accessor: 'name', // String-based value accessors!
+      width: 250, 
+      Cell: props => <a target="_blank" href="https://escapefromtarkov.gamepedia.com/{props.value}">{props.value}</a>
+    }, {
+      accessor: 'damage', // Required because our accessor is not a string
+      Header: 'Damage',
+      filterable: false,
+      filterall: false,
+      minWidth: 100,
+      sortFunc:  (a, b) => {
+                         if (a.length === b.length) {
+                           return a > b ? 1 : -1;
+                         }
+                         return a.length > b.length ? 1 : -1;
+                       }, 
+    }, {
+      accessor: 'armorpen', // Required because our accessor is not a string
+      Header: 'Armor Pen',
+      filterable: false,
+      filterall: false,
+      minWidth: 100,
+      sortFunc:  (a, b) => {
+                         if (a.length === b.length) {
+                           return a > b ? 1 : -1;
+                         }
+                         return a.length > b.length ? 1 : -1;
+                       }, 
+    }, {
+      accessor: 'speed', // Required because our accessor is not a string
+      Header: 'Speed',
+      filterable: false,
+      filterall: false,
+      minWidth: 100,
+      sortFunc:  (a, b) => {
+                         if (a.length === b.length) {
+                           return a > b ? 1 : -1;
+                         }
+                         return a.length > b.length ? 1 : -1;
+                       }, 
+    }, {
+      accessor: 'tracer', // Required because our accessor is not a string
+      Header: 'Tracer',
+      minWidth: 100,
+      Cell: ({ value }) => (value == 'FALSE' ? <span>X</span> : <span>✓</span>),
+      filterMethod: (filter, row) => {
+         if (filter.value === "all") {
+           return true;
+         }
+         if (filter.value === "true") {
+           return row[filter.id] != 'FALSE';
+         }
+         return row[filter.id] == "FALSE";
+       },
+       Filter: ({ filter, onChange }) =>
+         <select
+           onChange={event => onChange(event.target.value)}
+           style={{ width: "100%" }}
+           value={filter ? filter.value : "all"}
+         >
+           <option value="all">Show All</option>
+           <option value="true">Yes</option>
+           <option value="false">No</option>
+         </select>
+    }, {
+      accessor: 'armordmg', // Required because our accessor is not a string
+      Header: 'Armor Damage',
+      filterable: false,
+      filterall: false,
+      minWidth: 100,
+      sortFunc:  (a, b) => {
+                         if (a.length === b.length) {
+                           return a > b ? 1 : -1;
+                         }
+                         return a.length > b.length ? 1 : -1;
+                       }, 
+    }],
+  "dnr": [1,2,3,6,11]
+},{
+  "name":"Barter",
+  "color":"green",
+  "credit": "Which is maintained by /u/Gieke85", //<i>Which is maintained by <a href="https://www.reddit.com/user/Gieke85">/u/Gieke85</a></i>
+  "id":"1Yk-VriCy_8vDH4V9SsLwRYxem2mkzoDrULiaHZY5UGQ",
+  "range":"A3:L189",
+  "dnr": [3,4,5,7,8],
+  "header": [{
+      Header: 'Category',
+      accessor: 'cat', 
+      filterable: false,
+      filterall: false,
+      width: 100
+    }, {
+      Header: 'Item',
+      accessor: 'item',
+      width: 250, 
+      Cell: props => {let name = props.value.replace(/\([^)]*\)/gi,'');
+        let url = "https://escapefromtarkov.gamepedia.com/" + name;
+
+        return <a target="_blank" href={url}>{name}</a>}
+    }, {
+      accessor: 'price', // Required because our accessor is not a string
+      Header: 'Price',
+      minWidth: 100,
+      filterable: false,
+      filterall: false,
+        Cell: props => {
+        if (props.value[1] == props.value[2]) { 
+          return <span>{props.value[0]}₽</span>;
+          } else { 
+            return <span style={{display: "flex"}}>
+              <span style={{display: "none"}} title="Maximum Price" className="alert-success"><span className="glyphicon glyphicon-chevron-up" aria-hidden="true"></span><span className="sell-min">{props.value[1]} ₽</span></span><br />
+              <span title="Average Price" style={{cursor:"pointer"}} className="alert-info" onClick={priceExpand(this)}><span className="glyphicon glyphicon-minus" aria-hidden="true"></span><span className="sell-max">{props.value[2]} ₽</span></span><br />
+              <span style={{display: "none"}}title="Minimum Price" className="alert-danger"><span className="glyphicon glyphicon-chevron-down" aria-hidden="true"></span><span className="sell-avg">{props.value[0]} ₽</span></span>
+              </span>
+          }
+        }
+    }, {
+      accessor: 'value', // Required because our accessor is not a string
+      Header: 'Value',
+      minWidth: 100,
+      className: 'value-row',
+      filterable: false,
+      filterall: false,
+      Cell: props => { return <span>{props.value[1]}₽ ({props.value[2]}:{props.value[0]})</span>}
+    }, {
+      accessor: 'quest', // Required because our accessor is not a string
+      Header: 'Quest',
+      id: 'quest',
+      minWidth: 100,
+      Cell: ({ value }) => (value == '' || value == undefined ? "No" : value),
+      filterMethod: (filter, row) => {
+         if (filter.value === "all") {
+           return true;
+         }
+         if (filter.value === "true") {
+           return row[filter.id] != 'No' && row[filter.id] != undefined && row[filter.id] != "" ;
+         }
+         return row[filter.id] == "";
+       },
+       Filter: ({ filter, onChange }) =>
+         <select
+           onChange={event => onChange(event.target.value)}
+           style={{ width: "100%" }}
+           value={filter ? filter.value : "all"}
+         >
+           <option value="all">Show All</option>
+           <option value="true">Yes</option>
+           <option value="false">No</option>
+         </select>
+    }, {
+      accessor: 'trade', // Required because our accessor is not a string
+      Header: 'Tradeable',
+      minWidth: 100,
+      Cell: ({ value }) => (value == '' || value == undefined ? "No" : value),
+      filterMethod: (filter, row) => {
+         if (filter.value === "all") {
+           return true;
+         }
+         if (filter.value === "true") {
+           return row[filter.id] != 'No' && row[filter.id] != undefined && row[filter.id] != "" ;
+         }
+         return row[filter.id] == "";
+       },
+       Filter: ({ filter, onChange }) =>
+         <select
+           onChange={event => onChange(event.target.value)}
+           style={{ width: "100%" }}
+           value={filter ? filter.value : "all"}
+         >
+           <option value="all">Show All</option>
+           <option value="true">Yes</option>
+           <option value="false">No</option>
+         </select>
+    }, {
+      accessor: 'buy', // Required because our accessor is not a string
+      Header: 'Buyable',
+      minWidth: 100,
+      Cell: ({ value }) => (value == '' || value == undefined ? "No" : value),
+      filterMethod: (filter, row) => {
+         if (filter.value === "all") {
+           return true;
+         }
+         if (filter.value === "true") {
+           return row[filter.id] != 'No' && row[filter.id] != undefined && row[filter.id] != "" ;
+         }
+         return row[filter.id] == "";
+       },
+       Filter: ({ filter, onChange }) =>
+         <select
+           onChange={event => onChange(event.target.value)}
+           style={{ width: "100%" }}
+           value={filter ? filter.value : "all"}
+         >
+           <option value="all">Show All</option>
+           <option value="true">Yes</option>
+           <option value="false">No</option>
+         </select>
+    }]
+}];
+
+
+export default class CustomSortTable extends React.Component {
+  constructor(props) {
+      super();
+      this.state = {rows: [], columns: [], sheet: 1};
+
+      this.tableSwap = this.tableSwap.bind(this);
+  }
+
+  getGoog(sheetIndex) {
+    initGapi(sArray[sheetIndex].id, sArray[sheetIndex].range, (resp) => {
+      let range = resp.result;
+       if (range.values.length > 0) {
+         for (var i = 0; i < range.values.length; i++) {
+           var row = range.values[i];
+           var rowObj = [], columnObj = [];
+           var count = 0;
+
+            range.values.forEach(function(entry) {
+              if (count > 0) {
+                if (sheetIndex == 0) {
+                  rowObj.push({
+                    name: entry[0],
+                    price: parseInt(entry[1]),
+                    damage: parseInt(entry[2]),
+                    armorpen: parseInt(entry[3]),
+                    speed: parseInt(entry[4]),
+                    richo: entry[5],
+                    frag: entry[6],
+                    meta: parseInt(entry[8]),
+                  })
+                } else if (sheetIndex == 1) {
+                  rowObj.push({
+                    name: entry[0],
+                    damage: parseInt(entry[4]),
+                    armorpen: parseInt(entry[5]),
+                    armordmg: parseInt(entry[7]),
+                    tracer: entry[9],
+                    speed: parseInt(entry[10]),
+                  })
+                } else if (sheetIndex == 2) {
+                  rowObj.push({
+                    cat: entry[1],
+                    item: entry[0],
+                    price: [entry[2], parseInt(entry[3]),entry[4]],
+                    value: [ parseInt(entry[5]),entry[6],entry[7]],
+                    quest: entry[9],
+                    trade: entry[10],
+                    buy: entry[11],
+                  })
+                }
+              }
+              count++
+
+            });
+            this.setState({rows: rowObj, columns: sArray[sheetIndex].header}); 
+            }
+         }
+    }); 
+  }
+
+  componentDidMount() {
+    document.getElementById('MainMenu').style.display = 'block';
+    loadGapi();
+
+    if(this.props.pathname == 'bartering') {
+      this.getGoog(2);
+      this.setState({sheet: 2});
+      // document.getElementById('ammoTableSwapper').style.display = 'none';
+    } else {
+      this.getGoog(0);
+      this.setState({sheet: 0});
+    }
+  }
+
+  updateFooter(sheetIndex) {
+    var swap = document.getElementById('ammoTableSwapper');
+    var link = document.getElementById('ammoTableLink');
+
+    swap.innerHTML = 'Switch to '+ sArray[sheetIndex].name +' Data';
+    swap.style.color = sArray[sheetIndex].color;
+    link.href  = 'https://docs.google.com/spreadsheets/d/'+sArray[sheetIndex].id;
+  }
+
+  tableSwap(event) {
+    if (this.state.sheet == 0) {
+      this.getGoog(1);
+      this.updateFooter(0);
+      this.setState({sheet: 1});
+    } else {
+      this.getGoog(0);
+      this.updateFooter(1);
+      this.setState({sheet: 0});
+    }
+  }
+
+  render() {
+    const { rows, sheet, columns } = this.state;
+
+    return (
+      <div>
+          {rows.length == 0 &&
+          <div className="loading-div">Loading...</div>
+          }
+          {rows.length > 0 &&
+            <div className="jumbotron contentcontainer tab-container" id="Ammo" >
+              <ReactTable
+                data={rows}
+                columns={columns}
+                defaultPageSize={10}
+                className="-striped -highlight"
+                showPageSizeOptions={false}
+                resizable= {false}
+                showPageJump= {false}
+                filterable={true}
+                subComponent={row => {
+                  return(<div>Test</div>)
+                }}
+              />
+             <span>Data is pulled from the following <a id="ammoTableLink" target="_blank" href={'https://docs.google.com/spreadsheets/d/'+sArray[sheet].id}>spreadsheet</a>. <i>{sArray[sheet].credit}</i></span>
+             {sheet < 2 &&
+              <span>
+              <a id="ammoTableSwapper" style={{color: "gray", float: "right", cursor:"pointer"}} onClick={this.tableSwap}>Switch to Unofficial Data</a>
+              </span>
+              }
+             </div>
+            }
+    </div>);
+  }
+}
